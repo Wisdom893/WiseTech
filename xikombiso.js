386accdd748f07d121540a199c83e2adcd2dfe86
Wisdom893 <wxichavo@gmail.com>
2024-08-04 16:31:43 +0200

    Add files via upload

---
+++ xikombiso.js
@@ -0,0 +1,14 @@
+
+function myFunction()
+{
+  var fName=document.getElementById("sub1");
+  var exprience=document.getElementById("sub9")
+
+
+  var name=fName.value;
+  var exp=exprience.value;
+
+
+  document.write("<p>" +name+ "</p><p>" +exp+ "</p>"); 
+
+}
